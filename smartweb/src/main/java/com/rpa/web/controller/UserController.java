0ddf94b8f69bca505ce6f706481bc1103fbed2e3
hui.xia <hui.xia@rpahelper.com>
2019-09-25 19:49:51 +0800

    Construct code.

--- smartweb/src/main/java/com/rpa/web/controller/UserController.java
+++ smartweb/src/main/java/com/rpa/web/controller/UserController.java
@@ -1,20 +1,36 @@
 package com.rpa.web.controller;
 
-import org.springframework.stereotype.Controller;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.rpa.web.pojo.UserPO;
+import com.rpa.web.service.IUserService;
+import com.rpa.web.utils.DTPageInfo;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: xiahui
- * @date: Created in 2019/9/24 10:07
- * @description: TODO
+ * @date: Created in 2019/9/24 15:05
+ * @description: 用户信息
  * @version: 1.0
  */
-@Controller
+@RestController
 public class UserController {
+    @Resource
+    private IUserService service;
 
-    @RequestMapping("/userlist")
-    public String userList() {
-        return "userlist";
+    @RequestMapping("/user/list")
+    public DTPageInfo<UserPO> list(@RequestParam(value = "draw", defaultValue = "1") int draw, @RequestParam(value =
+            "start", defaultValue = "1") int pageNum,
+                                 @RequestParam(value = "length", defaultValue = "10") int pageSize) {
+        Page<UserPO> page = PageHelper.startPage(pageNum, pageSize);
+
+        List<UserPO> data = service.list();
+
+        return new DTPageInfo<>(draw, page.getTotal(), data);
     }
 }
-
