0cbbc2cab15eb8fac7e5d4431370b4596e929a87
hui.xia <hui.xia@rpahelper.com>
2019-09-30 11:48:36 +0800

    WxSupport complete.

---
+++ smartweb/src/main/java/com/rpa/web/service/IWxSupportService.java
@@ -0,0 +1,45 @@
+package com.rpa.web.service;
+
+import com.rpa.web.dto.WxSupportDTO;
+import com.rpa.web.utils.DTPageInfo;
+
+import java.util.Map;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2019/9/30 11:11
+ * @description: 微信白名单
+ * @version: 1.0
+ */
+public interface IWxSupportService {
+
+    /**
+     * 分页查询
+     *
+     * @param draw
+     * @param pageNum
+     * @param pageSize
+     * @param reqData
+     * @return
+     */
+    DTPageInfo<WxSupportDTO> query(int draw, int pageNum, int pageSize, Map<String, Object> reqData);
+
+
+    /**
+     * 插入
+     *
+     * @param packageName 包名
+     * @param extra       备注
+     * @return
+     */
+    int insert(String packageName, String extra);
+
+    /**
+     * 主键
+     *
+     * @param wId 主键
+     * @return
+     */
+    int delete(int wId);
+
+}
---
+++ smartweb/src/main/java/com/rpa/web/service/impl/WxSupportServiceImpl.java
@@ -0,0 +1,53 @@
+package com.rpa.web.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.rpa.web.dto.WxSupportDTO;
+import com.rpa.web.mapper.WxSupportMapper;
+import com.rpa.web.pojo.WxSupportPO;
+import com.rpa.web.service.IWxSupportService;
+import com.rpa.web.utils.DTPageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2019/9/30 11:11
+ * @description: 微信白名单
+ * @version: 1.0
+ */
+@Service
+public class WxSupportServiceImpl implements IWxSupportService {
+
+    @Resource
+    private WxSupportMapper wxSupportMapper;
+
+    @Override
+    public DTPageInfo<WxSupportDTO> query(int draw, int pageNum, int pageSize, Map<String, Object> reqData) {
+        Page<WxSupportPO> page = PageHelper.startPage(pageNum, pageSize);
+        List<WxSupportPO> pos = wxSupportMapper.query(reqData);
+        return new DTPageInfo<>(draw, page.getTotal(), WxSupportDTO.convert(pos));
+    }
+
+    @Override
+    public int insert(String packageName, String extra) {
+        WxSupportPO po = new WxSupportPO();
+
+        po.setPackageName(packageName);
+        po.setExtra(extra);
+        po.setCreateTime(new Date());
+
+        po.setaId(1);
+
+        return wxSupportMapper.insert(po);
+    }
+
+    @Override
+    public int delete(int wId) {
+        return wxSupportMapper.deleteByPrimaryKey(wId);
+    }
+}
