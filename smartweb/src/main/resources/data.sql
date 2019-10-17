169ff97dfd2a8a41b8a4085be60acfa0a1b70168
haoliang.cui <haoliang.cui@rpahelper.com>
2019-10-17 15:19:40 +0800

    添加应用渠道表的默认数据

--- smartweb/src/main/resources/data.sql
+++ smartweb/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into t_viptype(viptype_id,vipname) values(10,'会员');
 insert into t_viptype(viptype_id,vipname) values(20,'年费会员');
+insert into t_soft_channel(soft_channel_id,name,extra) values(1,"moren","moren");
 
 
