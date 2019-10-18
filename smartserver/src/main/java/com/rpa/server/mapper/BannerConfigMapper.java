bc6105fde4f516b78408501f7170908185abf1c6
hui.xia <hui.xia@rpahelper.com>
2019-10-18 09:28:17 +0800

    BannerConfig complete.

--- smartserver/src/main/java/com/rpa/server/mapper/BannerConfigMapper.java
+++ smartserver/src/main/java/com/rpa/server/mapper/BannerConfigMapper.java
@@ -3,9 +3,17 @@ package com.rpa.server.mapper;
 import com.rpa.server.pojo.BannerConfigPO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * BannerConfigMapper继承基类
  */
 @Mapper
 public interface BannerConfigMapper extends BaseMapper<BannerConfigPO, Integer> {
+    /**
+     * 查询所有数据
+     *
+     * @return
+     */
+    List<BannerConfigPO> queryAll();
 }
