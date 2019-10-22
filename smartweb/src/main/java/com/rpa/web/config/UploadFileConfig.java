09d6afba4a0b7ff0055e2434ab0f6e6e407b19a3
haoliang.cui <haoliang.cui@rpahelper.com>
2019-10-22 16:19:24 +0800

    更改静态资源映射路径

--- smartweb/src/main/java/com/rpa/web/config/UploadFileConfig.java
+++ smartweb/src/main/java/com/rpa/web/config/UploadFileConfig.java
@@ -30,7 +30,7 @@ public class UploadFileConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //文件磁盘图片url 映射
         //配置server虚拟路径，handler为前台访问的目录，locations为files相对应的本地路径
-        registry.addResourceHandler(staticAccessPath).addResourceLocations("file://" + uploadFolder);
+        registry.addResourceHandler(staticAccessPath).addResourceLocations("file:" + uploadFolder);
     }
 
     @Bean
