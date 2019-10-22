12db01830bf35eec794f589a6ef369ae8a0615f5
haoliang.cui <haoliang.cui@rpahelper.com>
2019-10-22 16:08:30 +0800

    获取app的渠道名

--- smartweb/src/main/java/com/rpa/web/utils/FileUtil.java
+++ smartweb/src/main/java/com/rpa/web/utils/FileUtil.java
@@ -9,6 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author: xiahui
@@ -131,11 +133,33 @@ public class FileUtil {
             apkFile.close();
             new File(filePath).renameTo(new File(newFile));
             apkInfo.put("url", newFile);
+            apkInfo.put("channel", getChannel(apkFile.getManifestXml()));
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return apkInfo;
+    }
+
+
+    /**
+     * 从manifest中获取UMENG_CHANNEL属性
+     *
+     * @param manifestXml
+     * @return
+     */
+    public static String getChannel(String manifestXml){
+        String pattern = "meta-data android:name=\"UMENG_CHANNEL\" android:value=\"([a-zA-Z0-9]*)";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(manifestXml);
+        if(m.find()){
+            String str = m.group(0);
+            String channel = str.substring(str.lastIndexOf("\"")+1);
+            return channel;
+        }
+        return null;
     }
 
     @Value("${file.uploadFolder}")
