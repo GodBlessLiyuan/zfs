05d726ddd353c29648fda469cced4cc6cbb492e4
hui.xia <hui.xia@rpahelper.com>
2019-10-25 20:10:27 +0800

    Income page struct.

--- smartfront/src/main/java/com/rpa/front/controller/PageController.java
+++ smartfront/src/main/java/com/rpa/front/controller/PageController.java
@@ -1,19 +1,36 @@
 package com.rpa.front.controller;
 
+import com.rpa.front.dto.IncomeDTO;
+import com.rpa.front.service.IIncomeService;
+import com.rpa.front.utils.VerifyUtil;
+import com.rpa.front.vo.IncomeVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author: xiahui
  * @date: Created in 2019/10/25 18:48
- * @description: TODO
+ * @description: 爱收益页面
  * @version: 1.0
  */
 @Controller
 public class PageController {
+    @Autowired
+    private IIncomeService service;
 
-    @RequestMapping("income")
-    public String income() {
+    @PostMapping("income")
+    public String income(@RequestBody IncomeDTO dto, ModelMap map, HttpServletRequest req) {
+        if (!VerifyUtil.checkToken(dto, req)) {
+            return null;
+        }
+        IncomeVO vo = service.query(dto);
+        map.put("res", vo);
+
         return "income_index";
     }
 }
---
+++ smartfront/src/main/java/com/rpa/front/service/impl/IncomeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.rpa.front.service.impl;
+
+import com.rpa.front.dto.IncomeDTO;
+import com.rpa.front.mapper.RevenueUserMapper;
+import com.rpa.front.pojo.RevenueUserPO;
+import com.rpa.front.service.IIncomeService;
+import com.rpa.front.vo.IncomeVO;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2019/10/25 19:16
+ * @description: 爱收益
+ * @version: 1.0
+ */
+@Service
+public class IncomeServiceImpl implements IIncomeService {
+    @Resource
+    private RevenueUserMapper revenueUserMapper;
+
+    @Override
+    public IncomeVO query(IncomeDTO dto) {
+        RevenueUserPO po = revenueUserMapper.selectByPrimaryKey(dto.getUd());
+
+        IncomeVO vo = new IncomeVO();
+        vo.setBalance(po.getRemaining());
+        vo.setInvitenum(po.getInviteCount());
+        vo.setPaynum(po.getPayCount());
+        vo.setTotalmny(po.getTotalRevenue());
+
+        return vo;
+    }
+}
