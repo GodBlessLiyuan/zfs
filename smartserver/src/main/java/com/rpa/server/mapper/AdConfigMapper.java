19eeec67f92ce9ebb7e4ad1c734b26ed1e76c6c1
dang <594294356@qq.com>
2019-10-29 17:43:11 +0800

    完成了功能视频接口

--- smartserver/src/main/java/com/rpa/server/mapper/AdConfigMapper.java
+++ smartserver/src/main/java/com/rpa/server/mapper/AdConfigMapper.java
@@ -3,9 +3,12 @@ package com.rpa.server.mapper;
 import com.rpa.server.pojo.AdConfigPO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * AdConfigMapper继承基类
  */
 @Mapper
 public interface AdConfigMapper extends BaseMapper<AdConfigPO, Integer> {
+    List<AdConfigPO> query();
 }
