d45f75588bfa902ed1a1d0f4dcf0d5c6aa28b459
dang <594294356@qq.com>
2019-10-29 11:10:09 +0800

    完成了支持微信辅助功能的应用

--- smartserver/src/main/java/com/rpa/server/mapper/WxSupportMapper.java
+++ smartserver/src/main/java/com/rpa/server/mapper/WxSupportMapper.java
@@ -3,9 +3,14 @@ package com.rpa.server.mapper;
 import com.rpa.server.pojo.WxSupportPO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * WxSupportMapper继承基类
  */
 @Mapper
 public interface WxSupportMapper extends BaseMapper<WxSupportPO, Integer> {
+    int queryMaxId();
+
+    List<String> queryPkgs();
 }
