964f771b9c9583f6e14f1c50bb33da1439679010
hui.xia <hui.xia@rpahelper.com>
2019-11-01 16:40:09 +0800

    RevenueUser add init.

---
+++ smartserver/src/main/java/com/rpa/server/dto/OrderFeedbackDTO.java
@@ -0,0 +1,14 @@
+package com.rpa.server.dto;
+
+import com.rpa.server.pojo.OrderFeedbackPO;
+import lombok.Data;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2019/11/1 15:03
+ * @description: 微信支付反馈表
+ * @version: 1.0
+ */
+@Data
+public class OrderFeedbackDTO extends OrderFeedbackPO {
+}
