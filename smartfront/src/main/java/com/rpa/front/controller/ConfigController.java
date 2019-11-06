a85e3036b576ad5af2ab54c1d6e47c8cc025fa47
haoliang.cui <haoliang.cui@rpahelper.com>
2019-11-06 20:08:30 +0800

    更新软件基础页面配置信息

---
+++ smartfront/src/main/java/com/rpa/front/controller/ConfigController.java
@@ -0,0 +1,66 @@
+package com.rpa.front.controller;
+
+import com.rpa.front.common.ErrorCode;
+import com.rpa.front.common.ResultVO;
+import com.rpa.front.constant.IncomeConstant;
+import com.rpa.front.dto.IncomeDTO;
+import com.rpa.front.dto.UserActivityDTO;
+import com.rpa.front.service.IIncomeService;
+import com.rpa.front.service.UserActivityService;
+import com.rpa.front.utils.VerifyUtil;
+import com.rpa.front.vo.IncomeVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: CHL
+ * @date: Created in 2019/10/25 18:48
+ * @description: 基础配置页面
+ * @version: 1.0
+ */
+@RequestMapping("v1.0")
+@Controller
+public class ConfigController {
+
+    @GetMapping("problem")
+    public String problem(@RequestBody IncomeDTO dto, ModelMap map, HttpServletRequest req) {
+        return "common_problem";
+    }
+
+    @GetMapping("freevip")
+    public String freevip(ModelMap map, HttpServletRequest req) {
+        return "freemember_index";
+    }
+
+    @GetMapping("memberrights")
+    public String viprights(ModelMap map, HttpServletRequest req) {
+        return "member_rights";
+    }
+
+    @GetMapping("protocol")
+    public String serviceprotocol(ModelMap map, HttpServletRequest req) {
+        return "member_service_protocol";
+    }
+
+    @GetMapping(value = "share/{shareCode}")
+    public String shareClick(@PathVariable String shareCode, ModelMap map) {
+        ResultVO vo = new ResultVO<>(1000, shareCode);
+        map.put("res", vo);
+        return "income_index";
+    }
+
+
+    /**
+     * @author: dangyi
+     * @description：免费领取会员：根据两个ID查询状态
+     */
+    @GetMapping("viprights")
+    public String freemember(@RequestBody UserActivityDTO dto, ModelMap map, HttpServletRequest req) {
+        return "settings_member_service_protocol";
+    }
+}
+
