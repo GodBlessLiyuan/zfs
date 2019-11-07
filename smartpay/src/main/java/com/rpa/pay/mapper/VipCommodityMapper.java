d64c9648178be9147bb772cfacb959906c29cfe4
haoliang.cui <haoliang.cui@rpahelper.com>
2019-11-07 11:47:56 +0800

    更改表

---
+++ smartpay/src/main/java/com/rpa/pay/mapper/VipCommodityMapper.java
@@ -0,0 +1,11 @@
+package com.rpa.pay.mapper;
+
+import com.rpa.pay.pojo.VipCommodityPO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * VipCommodityMapper继承基类
+ */
+@Mapper
+public interface VipCommodityMapper extends BaseMapper<VipCommodityPO, Integer> {
+}
