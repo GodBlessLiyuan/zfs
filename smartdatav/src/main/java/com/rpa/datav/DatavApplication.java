c83f13900c611af8bca5b16bff344cadd2da2380
hui.xia <hui.xia@rpahelper.com>
2019-11-21 19:29:52 +0800

    Rebuild server getOrder.

---
+++ smartdatav/src/main/java/com/rpa/datav/DatavApplication.java
@@ -0,0 +1,18 @@
+package com.rpa.datav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2019/11/21 11:29
+ * @description: 数据大脑
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class DatavApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatavApplication.class, args);
+    }
+}
