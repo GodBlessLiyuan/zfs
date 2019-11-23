4aabfd98d0e9cea0df40a991421bb2507e8ef8b8
hui.xia <hui.xia@rpahelper.com>
2019-11-23 15:30:04 +0800

    Rebuild detect.

--- smartdetect/src/main/java/com/rpa/detect/command/SmartFrontDetectRunnable.java
+++ smartdetect/src/main/java/com/rpa/detect/command/SmartFrontDetectRunnable.java
@@ -5,12 +5,12 @@ import com.rpa.detect.utils.DetectProcessUtil;
 /**
  * @author: xiahui
  * @date: Created in 2019/11/23 10:11
- * @description: 检测 smartweb 微服务
+ * @description: 检测 smartfront 微服务
  * @version: 1.0
  */
-public class SmartWebDetectRunnable implements Runnable {
+public class SmartFrontDetectRunnable implements Runnable {
     @Override
     public void run() {
-        DetectProcessUtil.detectProStatus("smartweb", 3);
+        DetectProcessUtil.detectProStatus("smartfront", 3);
     }
 }
