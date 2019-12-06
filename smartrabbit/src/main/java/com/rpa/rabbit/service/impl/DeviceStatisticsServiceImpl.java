5219db4ef1fa6bcd98a110e449c2ae0f3db1e0e5
dang <594294356@qq.com>
2019-12-06 16:54:20 +0800

    smartrabbit模块设备统计，在插入数据库前，要根据设备ID对设备的真实性进行判断

--- smartrabbit/src/main/java/com/rpa/rabbit/service/impl/DeviceStatisticsServiceImpl.java
+++ smartrabbit/src/main/java/com/rpa/rabbit/service/impl/DeviceStatisticsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.rpa.rabbit.service.impl;
 
+import com.rpa.rabbit.mapper.DeviceMapper;
 import com.rpa.rabbit.mapper.DeviceStatisticsMapper;
+import com.rpa.rabbit.pojo.DevicePO;
 import com.rpa.rabbit.pojo.DeviceStatisticsPO;
 import com.rpa.rabbit.service.DeviceStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,10 @@ public class DeviceStatisticsServiceImpl implements DeviceStatisticsService {
 
     @Autowired
     private DeviceStatisticsMapper deviceStatisticsMapper;
+
+    @Autowired
+    private DeviceMapper deviceMapper;
+
     @Autowired
     private StringRedisTemplate template;
 
@@ -38,25 +44,31 @@ public class DeviceStatisticsServiceImpl implements DeviceStatisticsService {
         Date visitTime = (Date) deviceInfo.get("visitTime");
         String ip = (String) deviceInfo.get("ip");
 
-        // 插入数据库
-        DeviceStatisticsPO po = new DeviceStatisticsPO();
-        po.setDeviceId(deviceId);
-        po.setVisitTime(visitTime);
-        po.setIp(ip);
-        this.deviceStatisticsMapper.insert(po);
+        //查询设备
+        DevicePO devicePO = this.deviceMapper.selectByPrimaryKey(deviceId);
+        //设备须真实存在
+        if (null != devicePO) {
+            // 插入数据库
+            DeviceStatisticsPO po = new DeviceStatisticsPO();
+            po.setDeviceId(deviceId);
+            po.setVisitTime(visitTime);
+            po.setIp(ip);
+            this.deviceStatisticsMapper.insert(po);
 
-        // 统计
-        int dayActiveUser = this.deviceStatisticsMapper.queryDayActiveUser();
-        int monthActiveUser = this.deviceStatisticsMapper.queryMonthActiveUser();
+            // 统计
+            int dayActiveUser = this.deviceStatisticsMapper.queryDayActiveUser();
+            int monthActiveUser = this.deviceStatisticsMapper.queryMonthActiveUser();
 
-        // 将统计结果封装成map
-        Map<String, String> deviceStatistics = new HashMap();
-        deviceStatistics.put("dayActiveUser", String.valueOf(dayActiveUser));
-        deviceStatistics.put("monthActiveUser", String.valueOf(monthActiveUser));
+            // 将统计结果封装成map
+            Map<String, String> deviceStatistics = new HashMap();
+            deviceStatistics.put("dayActiveUser", String.valueOf(dayActiveUser));
+            deviceStatistics.put("monthActiveUser", String.valueOf(monthActiveUser));
 
-        // 再存入Redis缓存
-        String current_date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        this.template.opsForHash().putAll("deviceStatistics" + current_date, deviceStatistics);
-        this.template.expire("deviceStatistics" + current_date, 25, TimeUnit.HOURS);
+            // 再存入Redis缓存
+            String current_date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            this.template.opsForHash().putAll("deviceStatistics" + current_date, deviceStatistics);
+            this.template.expire("deviceStatistics" + current_date, 25, TimeUnit.HOURS);
+        }
+
     }
 }
