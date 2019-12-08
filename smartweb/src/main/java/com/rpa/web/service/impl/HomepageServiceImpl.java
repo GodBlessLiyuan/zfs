2b671f230f96ef40a2a8dfff5fb38e22141069a2
dang <594294356@qq.com>
2019-12-08 20:13:27 +0800

    修改了各模块日志的配置，实现了日志分级输出

--- smartweb/src/main/java/com/rpa/web/service/impl/HomepageServiceImpl.java
+++ smartweb/src/main/java/com/rpa/web/service/impl/HomepageServiceImpl.java
@@ -5,7 +5,6 @@ import com.rpa.web.mapper.*;
 import com.rpa.web.service.HomepageService;
 import com.rpa.web.utils.ResultVOUtil;
 import com.rpa.web.vo.ResultVO;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
