6a565d6445a10c0b1c44513eb2b7685c93d732f8
hui.xia <hui.xia@rpahelper.com>
2019-12-09 15:15:33 +0800

    Rebuild smartkafka, @value kafka default topic.

--- smartkafka/src/main/java/com/rpa/kafka/service/KafkaService.java
+++ smartkafka/src/main/java/com/rpa/kafka/service/KafkaService.java
@@ -3,6 +3,7 @@ package com.rpa.kafka.service;
 import com.alibaba.fastjson.JSON;
 import com.rpa.common.utils.RequestUtil;
 import com.rpa.kafka.common.ResultVO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,8 @@ import java.util.Map;
  */
 @Service
 public class KafkaService {
+    @Value("${kafka.default.topic}")
+    private String topic;
 
     @Resource
     private KafkaTemplate<String, String> kafkaTemplate;
@@ -27,7 +30,7 @@ public class KafkaService {
         Map<String, Object> msg = new HashMap<>(1);
         msg.put("ip", RequestUtil.getIpAddr(req));
         msg.put("data", data);
-        this.kafkaTemplate.send("smart", JSON.toJSONString(msg));
+        this.kafkaTemplate.send(topic, JSON.toJSONString(msg));
 
         return new ResultVO(1000);
     }
