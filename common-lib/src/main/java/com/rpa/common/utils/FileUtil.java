6328cceaf89d338742034599d0952e6b0e2a8819
xiahui <hui.xia@rpahelper.com>
2020-01-03 17:01:16 +0800

    Rebuild FileUtil.

--- common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
+++ common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
@@ -141,7 +141,7 @@ public class FileUtil {
 //            modifyApkIcon(zipPath, pic, suffix);
             modifyApkName(xmlUrl, name, zipPath);
 //            modifyApkPkg(xmlUrl, pkg, zipPath);
-//            modifyApkSign(zipPath);
+            modifyApkSign(zipPath);
         } catch (Exception e) {
             logger.error(e.getMessage());
             e.printStackTrace();
