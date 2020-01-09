db3a4e7d14a80446b78a2e90f9024ad3e066ab8f
xiahui <hui.xia@rpahelper.com>
2020-01-09 20:07:58 +0800

    Rebuild phone model.

---
+++ common-lib/src/main/java/com/rpa/common/bo/PhoneModelBO.java
@@ -0,0 +1,15 @@
+package com.rpa.common.bo;
+
+import com.rpa.common.pojo.PhoneModelPO;
+import lombok.Data;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/1/9 19:56
+ * @description: TODO
+ * @version: 1.0
+ */
+@Data
+public class PhoneModelBO extends PhoneModelPO {
+    private String typeName;
+}
--- dkfsserver/src/main/java/com/rpa/server/service/impl/PhoneModelServiceImpl.java
+++ dkfsserver/src/main/java/com/rpa/server/service/impl/PhoneModelServiceImpl.java
@@ -1,8 +1,13 @@
 package com.rpa.server.service.impl;
 
+import com.rpa.common.bo.PhoneModelBO;
+import com.rpa.common.mapper.PhoneModelMapper;
 import com.rpa.common.vo.ResultVO;
 import com.rpa.server.service.IPhoneModelService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.*;
 
 /**
  * @author: xiahui
@@ -13,8 +18,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class PhoneModelServiceImpl implements IPhoneModelService {
 
+    @Resource
+    private PhoneModelMapper phoneModelMapper;
+
     @Override
     public ResultVO phoneModel() {
-        return null;
+        List<PhoneModelBO> bos = phoneModelMapper.queryAll();
+        Map<String, List<String>> data = new LinkedHashMap<>();
+        for (PhoneModelBO bo : bos) {
+            if (data.containsKey(bo.getTypeName())) {
+                List<String> list = data.get(bo.getTypeName());
+                list.add(bo.getName());
+            } else {
+                List<String> list = new LinkedList<>();
+                list.add(bo.getName());
+                data.put(bo.getTypeName(), list);
+            }
+        }
+
+        return new ResultVO<>(1000, data);
     }
 }
