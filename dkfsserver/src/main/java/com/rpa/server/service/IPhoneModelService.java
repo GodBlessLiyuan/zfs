de5f7aa2b98dfc55d8e8805951618ec3928a1ae1
xiahui <hui.xia@rpahelper.com>
2020-01-09 19:32:37 +0800

    Rebuild dkfs, add phone model and type.

---
+++ dkfsserver/src/main/java/com/rpa/server/service/IPhoneModelService.java
@@ -0,0 +1,18 @@
+package com.rpa.server.service;
+
+import com.rpa.common.vo.ResultVO;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/1/9 14:47
+ * @description: 手机型号
+ * @version: 1.0
+ */
+public interface IPhoneModelService {
+    /**
+     * 获取手机型号
+     *
+     * @return
+     */
+    ResultVO phoneModel();
+}
---
+++ dkfsserver/src/main/java/com/rpa/server/service/impl/PhoneModelServiceImpl.java
@@ -0,0 +1,20 @@
+package com.rpa.server.service.impl;
+
+import com.rpa.common.vo.ResultVO;
+import com.rpa.server.service.IPhoneModelService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/1/9 14:47
+ * @description: 手机型号
+ * @version: 1.0
+ */
+@Service
+public class PhoneModelServiceImpl implements IPhoneModelService {
+
+    @Override
+    public ResultVO phoneModel() {
+        return null;
+    }
+}
