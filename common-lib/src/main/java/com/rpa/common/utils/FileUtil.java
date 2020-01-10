b03c17b0ce7cf675dc16b4a997b432e675a76638
xiahui <hui.xia@rpahelper.com>
2020-01-10 18:18:18 +0800

    Rebuild dkfs fileUtil.

--- common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
+++ common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
@@ -253,7 +253,7 @@ public class FileUtil {
 
         // 修改2处 meta-data
         FileUtil.modifyApkPkg("meta-data", 1, "value", "1111111111111111", xmlPath, outXml);
-//        FileUtil.modifyApkPkg("meta-data", 2, "value", "2222222222222222", xmlPath, outXml);
+        FileUtil.modifyApkPkg("meta-data", 2, "value", "2222222222222222", xmlPath, outXml);
 
         return true;
     }
