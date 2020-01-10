d064cf9a667c25b34fe4f32100b6d64ea4e96bd8
xiahui <hui.xia@rpahelper.com>
2020-01-10 18:58:17 +0800

    Rebuild dkfs fileUtil.

--- common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
+++ common-lib/src/main/java/com/rpa/common/utils/FileUtil.java
@@ -153,12 +153,12 @@ public class FileUtil {
         boolean isModApkName = modifyApkName(tempXmlUrl, dto.getName());
         boolean isModApkPkg = modifyApkPkg(tempXmlUrl, dto, tempFilePath);
         String templateUrl = templatePath + ModuleConstant.AVATAR + random + ".apk";
-        modifyApkSign(rootDir + templateUrl, tempFilePath, isModApkName || isModApkPkg);
+//        modifyApkSign(rootDir + templateUrl, tempFilePath, isModApkName || isModApkPkg);
 
         // 清除临时文件夹
-        String[] CMD_STR = new String[]{"/bin/sh", "-c", "cd " + rootDir + templatePath + "; rm -rf " + random};
-        Process process = Runtime.getRuntime().exec(CMD_STR);
-        process.waitFor();
+//        String[] CMD_STR = new String[]{"/bin/sh", "-c", "cd " + rootDir + templatePath + "; rm -rf " + random};
+//        Process process = Runtime.getRuntime().exec(CMD_STR);
+//        process.waitFor();
 
         return templateUrl;
     }
