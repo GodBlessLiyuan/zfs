d8b6a0303fe441a149d3b065c8870e718099afc0
xiahui <hui.xia@rpahelper.com>
2020-05-13 10:38:11 +0800

    Rebuild dkfs, add om.

---
+++ dkfsfront/src/main/java/com/rpa/front/controller/OMController.java
@@ -0,0 +1,35 @@
+package com.rpa.front.controller;
+
+import com.rpa.front.service.IOMService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: xiahui
+ * @date: Created in 2020/5/13 9:37
+ * @description: 运营需求
+ * @version: 1.0
+ */
+@RequestMapping("om")
+@Controller
+public class OMController {
+    @Autowired
+    private IOMService iomService;
+
+    /**
+     * 查找 官网 最新 发布 APP 包
+     *
+     * @return
+     */
+    @RequestMapping(value = "findLatest", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String find(HttpServletResponse res) throws IOException {
+        return iomService.findMaxVersionAPP(res);
+    }
+}
