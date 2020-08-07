06ca17f40360a9ace0c4d10b4f36dc44eb847dae
liyuan <1627739285@qq.com>
2020-08-07 11:47:48 +0800

    修改device bug

--- zfsserver/src/main/java/com/zfs/server/service/impl/DeviceServiceImpl.java
+++ zfsserver/src/main/java/com/zfs/server/service/impl/DeviceServiceImpl.java
@@ -57,16 +57,17 @@ public class DeviceServiceImpl implements IDeviceService {
 
             return this.buildResultVO(po.getDeviceId());
         }
-
+        List<String> resultImei=new ArrayList<>(imeis.size());
         // 过滤imei号长度<13
         for (String imei : imeis) {
             if (imei.length() < 13) {
-                imeis.remove(imei);
+            }else{
+                resultImei.add(imei);
             }
         }
 
 
-        List<Long> deviceIds = deviceImeiMapper.queryDevIdsByImei(imeis);
+        List<Long> deviceIds = deviceImeiMapper.queryDevIdsByImei(resultImei);
         if (null == deviceIds || deviceIds.size() == 0) {
             logger.warn("DeviceIds size: " + (null == deviceIds ? null : deviceIds.size()));
             // 没有查询到相关设备信息
