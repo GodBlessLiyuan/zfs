7bec3e83575a6b17a93a261a1cb95556545e0af2
liyuan <1627739285@qq.com>
2020-08-11 18:46:27 +0800

    会员卡配置的实现-定时任务

---
+++ zfsweb/src/main/java/com/zfs/web/controller/ChBatchScheduleTestController.java
@@ -0,0 +1,24 @@
+package com.zfs.web.controller;
+
+import com.zfs.common.vo.ResultVO;
+import com.zfs.web.service.impl.ChBatchScheduleServiceImpl;
+import org.modelmapper.internal.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: liyuan
+ * @data 2020-08-11 18:41
+ */
+//@RestController
+public class ChBatchScheduleTestController {
+//    @Autowired
+//    private ChBatchScheduleServiceImpl service;
+//    @PostMapping("chBatchScheduleTest")
+//    public ResultVO chBatchScheduleTest(){
+//        service.overLineCount();
+//        return new ResultVO(1000);
+//    }
+}
