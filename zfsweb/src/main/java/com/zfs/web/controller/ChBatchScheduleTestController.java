ea29a8b9e979a7b89fea69fe2ed966dc35ba206f
liyuan <1627739285@qq.com>
2020-08-17 09:02:40 +0800

    batchinfo忘记注销

--- zfsweb/src/main/java/com/zfs/web/controller/ChBatchScheduleTestController.java
+++ zfsweb/src/main/java/com/zfs/web/controller/ChBatchScheduleTestController.java
@@ -2,9 +2,9 @@ package com.zfs.web.controller;
 
 import com.zfs.common.vo.ResultVO;
 import com.zfs.web.service.impl.ChBatchScheduleServiceImpl;
+import org.modelmapper.internal.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: liyuan
  * @data 2020-08-11 18:41
  */
-@RestController
+//@RestController
 public class ChBatchScheduleTestController {
-    @Autowired
-    private ChBatchScheduleServiceImpl service;
-    @PostMapping("chBatchScheduleTest")
-    public ResultVO chBatchScheduleTest(){
-        service.overLineCount();
-        return new ResultVO(1000);
-    }
+//    @Autowired
+//    private ChBatchScheduleServiceImpl service;
+//    @PostMapping("chBatchScheduleTest")
+//    public ResultVO chBatchScheduleTest(){
+//        service.overLineCount();
+//        return new ResultVO(1000);
+//    }
 }
